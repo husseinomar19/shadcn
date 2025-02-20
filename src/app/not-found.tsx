a4bdf768f185bd802190892bdf0e98a6578f06d3
Hussein <omerhussen603@gmail.com>
2025-02-20 15:50:30 +0100

    update file's

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function Notfound(){
+    return(
+        <>
+        <div className="h-screen w-screen m-auto">
+           <h1 className="text-[50px]">404</h1>
+        </div>
+        </>
+    )
+}
